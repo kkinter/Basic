f18e375900b2e9382409c25d2d994206b1952144
wook <ifol1129@gmail.com>
2022-09-22 19:25:29 +0900

    0922 ADD

--- framework/0922/django_0922/django_0922/settings.py
+++ framework/0922/django_0922/django_0922/settings.py
@@ -11,18 +11,34 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, json
+
+
+from django.core.exceptions import ImproperlyConfigured
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
+with open(secret_file, 'r') as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0=1$4t*wil-09kfr*6quyvvc+etud#28i7509fzcmc8hri028u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
